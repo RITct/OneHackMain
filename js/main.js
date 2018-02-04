9f126aa53d848e0f5a6e829804c0c44e22f8290a
vijaicv <vijaicv@gmail.com>
2018-02-04 14:59:50 +0530

    jquery control

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-  var preload = new createjs.LoadQueue();
-  preload.addEventListener("fileload", handleFileComplete);
-  preload.loadFile("/img/intro-carousel/1.jpg");
-  preload.loadFile("/img/intro-carousel/2.jpg");
-  preload.loadFile("/img/intro-carousel/3.jpg");
-  preload.loadFile("/img/intro-carousel/4.jpg");
-  
-
-
-  function handleFileComplete(){
-    setTimeout(function() {
-      $('#wrapper').removeClass('loading');
-      $('#wrapper').addClass('loaded');
-      
-    }, 1000);
-  }
 
 
 jQuery(document).ready(function( $ ) {
-  
+  setTimeout(function() {
+    $('#wrapper').removeClass('loading');
+    $('#wrapper').addClass('loaded');
+    
+  }, 1000);
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
