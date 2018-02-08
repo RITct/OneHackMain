5399c98e257c9c8bb501b4ab009f91bb4201b723
vijaicv <vijaicv@gmail.com>
2018-02-08 18:37:06 +0530

    sponsors added

--- js/main.js
+++ js/main.js
@@ -166,21 +166,13 @@ jQuery(document).ready(function( $ ) {
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
 
-  // Clients carousel (uses the Owl Carousel library)
+  // sponsors carousel (uses the Owl Carousel library)
   $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    center:true,
+    responsive: { 0: { items: 1 }, 768: { items: 4 }, 900: { items: 6 }
     }
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+
 
 });
